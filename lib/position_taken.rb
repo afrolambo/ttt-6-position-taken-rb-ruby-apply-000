82eed95d58d58128de724a5c395d1c8d9a203cf8
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-09 17:52:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board,index)
   board = [" "," "," "," "," "," "," "," "," "]
   index = 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8
-  if board[index] == "X" || "O"
+  if board[index] == "X" || board[index] ==  "O"
     return true
   else board[index] == " " || "" || nil
   return false
