4a9a4bdd9fc69632fee36044f548e0ad5d361c94
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-09 17:17:56 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -7,6 +7,8 @@ end
 def position_taken?(board,index = "X")
   if board[index] == "X"
     return true
+  elsif index == user_input - 1
+    return true
   else board[index] == " " || "" || nil
     return false
   end
