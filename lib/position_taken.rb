6bb32641a95dc0a71c57f0caedd2e96ca7dfa6ae
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-09 17:51:16 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,8 @@
 # code your #position_taken? method here!
-board = [" "," "," "," "," "," "," "," "," "]
-index = 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8
+
 def position_taken?(board,index)
+  board = [" "," "," "," "," "," "," "," "," "]
+  index = 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8
   if board[index] == "X" || "O"
     return true
   else board[index] == " " || "" || nil
